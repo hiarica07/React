3b59fee7497ee3427e410ef6aa03305b2c7cad6b
hiarica07 <halilarica@gmail.com>
2024-07-03 16:34:59 +0300

    Test

---
+++ my-react2/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+<<<<<<< HEAD
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DE09</h1>
+=======
+import Msg from "./msg/Msg";
+
+function App() {
+  return (
+    <div>
+      <Msg/>
+
+
+
+>>>>>>> 58d18111c9908eddf18a4d3cc5f2e7de37fc0127
+    </div>
+  );
+}
+
+export default App;
