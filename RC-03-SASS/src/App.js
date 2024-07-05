f0c6da7c2d0babd30d62e20bb88f5430402666b8
hiarica07 <halilarica@gmail.com>
2024-07-05 10:36:01 +0300

    check

--- RC-03-SASS/src/App.js
+++ RC-03-SASS/src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>DE09</h1>
+      <Header />
     </div>
   );
 }
 
 export default App;
+
