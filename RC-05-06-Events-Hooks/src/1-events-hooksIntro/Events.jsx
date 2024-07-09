2bc36cf6ab7f860b23dc85fa7162c28d8d8ec067
hiarica07 <halilarica@gmail.com>
2024-07-09 10:59:11 +0300

    check

--- RC-05-06-Events-Hooks/src/1-events-hooksIntro/Events.jsx
+++ RC-05-06-Events-Hooks/src/1-events-hooksIntro/Events.jsx
@@ -9,6 +9,16 @@ const Events = () => {
       console.log(count);
       document.querySelector("span").textContent=count
     }
+//! react alanında parametreli fonksiyon çağıracaksak, fonksiyonun adının önüne ()=> işaretini koymalıyız, yoksa fonksiyonu event ı beklemeden çalıştırıyor
+    const butonTitle=(title)=>{
+      console.log(title);
+      document.querySelector(".btn-danger").textContent=title
+    }
+
+    const baslikChange=(yeni)=>{
+      baslik = yeni
+      document.querySelector("h1").textContent=yeni
+    }
 
   return (
 
@@ -16,6 +26,9 @@ const Events = () => {
     <h1> INFO : {baslik}</h1>
     <h2> COUNT: <span className='text-danger'>{count}</span></h2>
     <button onClick={arttir} className='btn btn-primary'>ARTTIR</button>
+    <button onClick={()=>butonTitle("halil")} className='btn btn-danger m2'>DEGISTIR</button>
+    <button onClick={()=>baslikChange("MerhabaKral")} className='btn btn-info'>TIKLANDI</button>
+    
     </div>
 
   )
