3322297aa1995e43bdc648a41bba6ac6197b9451
hiarica07 <halilarica@gmail.com>
2024-07-17 10:23:39 +0300

    check

---
+++ RC-09-HospitalApp/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <header className="header">
+          <h1>HOSPITAL</h1>
+
+          <div className="dr">doktorlar</div>
+        </header>
+
+        <div>hastaekle</div>
+
+      </div>
+
+      hastaliste
+    </div>
+  );
+};
+
+export default Home;
