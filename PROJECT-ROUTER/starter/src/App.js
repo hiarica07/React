a165d70dcd86be342d595e340fdc952f6725c0dd
hiarica07 <halilarica@gmail.com>
2024-07-23 15:35:28 +0300

    check

--- PROJECT-ROUTER/starter/src/App.js
+++ PROJECT-ROUTER/starter/src/App.js
@@ -1,14 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Html from "./pages/Html";
+import Css from "./pages/Css"
+import Logo from "./pages/Logo"
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/htmll" element={<Html />}/>
+        <Route path="/css" element={<Css />}/>
+        <Route path="/logo" element={<Logo />}/>
+      </Routes>
+      <Footer/>
+    </Router>
     </>
   );
 }
