5f343d3490b846827b348162c55db3c4fa12f4ec
hiarica07 <halilarica@gmail.com>
2024-07-23 12:52:56 +0300

    isActive attributes

--- RC-12-13react-Router/src/App.js
+++ RC-12-13react-Router/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; // BrowserRouter --> Router icin as Router yazildi.
+import MyNavbar from "./components/MyNavbar";
+import Teacher from "./pages/Teacher"
+import CourseCard from "./pages/CourseCard"
+import Paths from "./pages/Paths";
+import ContactForm from "./pages/ContactForm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>DE09</h1>
+    <div>
+      <Router>
+      <MyNavbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/teacher" element={<Teacher />} />
+          <Route path="/courses" element={<CourseCard/>} />
+          <Route path="/contact" element={<ContactForm/>} />
+          <Route path="/paths" element={<Paths/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- RC-12-13react-Router/src/components/MyNavbar.jsx
+++ RC-12-13react-Router/src/components/MyNavbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Container, Nav, Navbar, NavDropdown,} from 'react-bootstrap'
+import logo from "../img/logo.png"
+import { Link, NavLink } from 'react-router-dom'
 
 const MyNavbar = () => {
   return (
-    <div>MyNavbar</div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+       <Link to="/">
+        <img src={logo} alt="" />
+       </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink style={({isActive})=>({color:isActive && "red"})} className="nav-link" to="">Home</NavLink>
+            <NavLink style={({isActive})=>({fontSize:isActive && "20px"})} className="nav-link" to="/teacher">Teacher</NavLink>
+            <NavLink style={({isActive})=>({background:isActive && "aqua"})} className="nav-link" to="/courses">Courses</NavLink>
+            <NavLink style={({isActive})=>({border:isActive && "1px solid blue"})} className="nav-link" to="/contact">Contact</NavLink>
+            <NavLink style={({isActive})=>({color:isActive && "red"})} className="nav-link" to="/paths">Paths</NavLink>
+            <Nav.Link href="https://github.com/" target='blank'>GitHub</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
