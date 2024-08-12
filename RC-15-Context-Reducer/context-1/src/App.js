b72e978dded86539673d5995b8a3fefd298a9219
hiarica07 <halilarica@gmail.com>
2024-08-12 11:18:58 +0300

    Practise one way to useContext

--- RC-15-Context-Reducer/context-1/src/App.js
+++ RC-15-Context-Reducer/context-1/src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import Home from './components/Home'
+import data from "./data"
+
+//! 1-context alani aciyoruz.
+
+export const StudentContext =createContext() 
 
 const App = () => {
+
+const [students, setStudents] = useState(data)
+const changeColor=(id,newRenk)=>{}
+  
   return (
-    <div>App</div>
+
+   //! 2- Bütün projeye gönderilmek üzere ilk Home sayfasını gönderilecek veriler ile sarmalladık.
+
+
+    <StudentContext.Provider value={{students,changeColor}}>
+
+    <Home/>
+
+    </StudentContext.Provider>
   )
 }
 
--- RC-15-Context-Reducer/context-1/src/components/OgrenciItem.jsx
+++ RC-15-Context-Reducer/context-1/src/components/OgrenciItem.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { StudentContext } from "../App";
 
 const OgrenciItem = () => {
-  return (
-    <div>OgrenciItem</div>
-  )
-}
+  //! 3- (context consumer) globalde (mesela App.js de) tanımladığım değerleri buraya çağırdık.
 
-export default OgrenciItem
+  const { students,changeColor } = useContext(StudentContext);
+
+  return (
+    <div>
+      {students.map((i) => (
+        <div style={{backgroundColor:i.color}}>
+          <h3>NAME:{i.name}</h3>
+          <h4>EMAIL:{i.email}</h4>
+          <h4>AGE:{i.age}</h4>
+          Color: <input type="text" value={i.color} onChange={(e)=> changeColor(i.id, e.target.value)}/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default OgrenciItem;
